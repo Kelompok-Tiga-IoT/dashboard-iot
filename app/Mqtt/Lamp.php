ed9b12867b69d6bd9a8852dae35e3a8a08b36241
Naufal Reky Ardhana <rekyardhana029@gmail.com>
2024-09-23 10:59:02 +0700

    feat: Lamp sensor controller

---
+++ app/Mqtt/Lamp.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mqtt;
+
+use PhpMqtt\Client\Facades\MQTT;
+
+class Lamp extends AbstractSensors
+{
+    /*
+     * iot/sensor/lamp/+
+     * iot/sensor/lamp/1
+     */
+    public static string $topic = 'iot/sensor/lamp/+';
+
+
+    static function process(string $topic, array $data)
+    {
+        echo $topic . json_encode($data) . PHP_EOL;
+    }
+
+    static function turnOn($lamp_id): void
+    {
+        self::send('iot/sensor/lamp/' . $lamp_id, ['action' => 'on']);
+    }
+
+    static function turnOff($lamp_id): void
+    {
+        self::send('iot/sensor/lamp/' . $lamp_id, ['action' => 'off']);
+    }
+
+    static function getStatus($lamp_id): void
+    {
+        self::send('iot/sensor/lamp/' . $lamp_id, ['action' => 'status']);
+    }
+}
